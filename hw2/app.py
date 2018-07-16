8414f2908467865c6a215da1c63b65e27ac46f14
KevYoung <KevYoung@pdx.edu>
2018-07-15 23:48:21 -0700

    Adding comments to all .py code

--- hw2/app.py
+++ hw2/app.py
@@ -1,16 +1,20 @@
+""" This is a Movie Review flask application """
 from flask import Flask, redirect, request, url_for, render_template
 import MRModels
 
 app = Flask(__name__)
+model = MRModels.get_model()
 
+""" Function decorator using app.route ( '/', index() ) """
 @app.route('/')
 @app.route('/index.html')
 def index():
+    """ Main movie review webpage """
     return render_template('index.html')
 
 @app.route('/reviews')
 def reviews():
-    model = MRModels.get_model()
+    """ Displays all the movie reviews within a webpage """
     reviews = model.select()
     return render_template('reviews.html', reviews=reviews)
 
