b19962f9d8a76ff53805350ee2917eb5702b7040
KevYoung <KevYoung@pdx.edu>
2018-07-18 22:53:41 -0700

    Updated movie review form

--- hw3/MRModels/model_sqlite3.py
+++ hw3/MRModels/model_sqlite3.py
@@ -25,5 +25,13 @@ class model(Model):
         cursor.execute("SELECT * FROM reviews")
         return cursor.fetchall()
 
-    def insert(self):
+    def insert(self, movie, year, genre, rating, review, reviewer):
+        values = {'movie':movie, 'year':year, 'genre':genre, 'rating':rating,
+        'review':review, 'reviewer':reviewer}
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("insert into reviews (movie, year, genre, rating, review, reviewer) VALUES (:movie, :year, :genre, :rating, :review, :reviewer)", values)
+
+        connection.commit()
+        cursor.close()
         return True
--- hw3/app.py
+++ hw3/app.py
@@ -18,9 +18,11 @@ def reviews():
     reviews = [dict(movie=row[0], year=row[1], genre=row[2], rating=row[3], review=row[4], reviewer=row[5]) for row in model.select()]
     return render_template('reviews.html', reviews=reviews)
 
-@app.route('/newreview')
+@app.route('/newreview', methods=['GET','POST'])
 def newreview():
     """ Webpage to add a new movie review """
+    #model.insert(request.form['movie'], request.form['year'], request.form['genre'],
+    #request.form['rating'], request.form['review'], request.form['reviewer'])
     return render_template('newreview.html')
 
 if __name__ == '__main__':
