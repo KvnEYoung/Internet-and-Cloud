b7797a9325a7a916000ddc9b7db383796bf744d9
KevYoung <KevYoung@pdx.edu>
2018-07-20 00:17:26 -0700

    adjusted pydict model to work with reviews and newreview pages

--- hw3/MRModels/model_pydict.py
+++ hw3/MRModels/model_pydict.py
@@ -3,21 +3,27 @@ from .Model import Model
 
 class model(Model):
     def __init__(self):
-        """ Initializes the moviereviews dictionary and then calls the insert function """
+        """ Initializes the moviereviews dictionary and then adds the hardcoded movie reviews """
         self.moviereviews = {}
-        self.insert()
+        self.moviereviews["movie0"] = { "movie":"The Purge", "year":2013, "genre":"Horror", "rating":3,"review":"Horrible", "reviewer": "John Doe"}
+        self.moviereviews["movie1"] = { "movie":"Star Wars", "year":1978, "genre":"Sci-Fi", "rating":10,"review":"Excellent", "reviewer": "Johnny Doe"}
+        self.moviereviews["movie2"] = { "movie":"Avengers", "year":2012, "genre":"Sci-Fi", "rating":9,"review":"Very good", "reviewer": "Jane Doe"}
+        self.moviereviews["movie3"] = { "movie":"Deadpool", "year":2016, "genre":"Sci-Fi", "rating":9,"review":"Great movie", "reviewer": "Jimmy Doe"}
+        self.moviereviews["movie4"] = { "movie":"Harry Potter and the Sorcerer's Stone", "year":2001, "genre":"Fantasy", "rating":10,"review":"Fantastic", "reviewer": "Jonathan Doe"}
+        self.moviereviews["movie5"] = { "movie":"Pitch Perfect", "year":2012, "genre":"Comedy", "rating":8,"review":"Funny movie", "reviewer": "Ginger Doe"}
 
     def select(self):
         """ Returns all the moviereviews dictionary entries, with each entry containing
         the movie title, year, genre, rating, review and reviewer """
-        return self.moviereviews
+        templist = []
+        for key in self.moviereviews.keys():
+            review = self.moviereviews[key]
+            tempreview = (review['movie'], review['year'], review['genre'], review['rating'], review['review'], review['reviewer'])
+            templist.append(tempreview)
+        return templist
 
-    def insert(self):
+    def insert(self, movie, year, genre, rating, review, reviewer):
         """ Inserts movie reviews (title, year, genre, rating, review and reviewer) into the moviereview dictionary """
-        self.moviereviews["movie1"] = { "movie":"The Purge", "year":2013, "genre":"Horror", "rating":3,"review":"Horrible", "reviewer": "John Doe"}
-        self.moviereviews["movie2"] = { "movie":"Star Wars", "year":1978, "genre":"Sci-Fi", "rating":10,"review":"Excellent", "reviewer": "Johnny Doe"}
-        self.moviereviews["movie3"] = { "movie":"Avengers", "year":2012, "genre":"Sci-Fi", "rating":9,"review":"Very good", "reviewer": "Jane Doe"}
-        self.moviereviews["movie4"] = { "movie":"Deadpool", "year":2016, "genre":"Sci-Fi", "rating":9,"review":"Great movie", "reviewer": "Jimmy Doe"}
-        self.moviereviews["movie5"] = { "movie":"Harry Potter and the Sorcerer's Stone", "year":2001, "genre":"Fantasy", "rating":10,"review":"Fantastic", "reviewer": "Jonathan Doe"}
-        self.moviereviews["movie6"] = { "movie":"Pitch Perfect", "year":2012, "genre":"Comedy", "rating":8,"review":"Funny movie", "reviewer": "Ginger Doe"}
+        movienumber = "movie" + str(len(self.moviereviews.keys()))
+        self.moviereviews[movienumber] = { "movie":movie, "year":year, "genre":genre, "rating":rating, "review":review, "reviewer":reviewer}
         return True
