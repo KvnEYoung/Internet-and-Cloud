041d7ce3d985f56b87b472e99203ef5e21ca9a70
KevYoung <kevyoung@pdx.edu>
2018-08-09 20:42:16 -0700

    Modifying and removing flask views.

--- final/main.py
+++ final/main.py
@@ -4,17 +4,55 @@
 Movie review flask app
 '''
 
-from flask import Flask, url_for, render_template
-from flask.views import MethodView
-from index import index
-from reviews import reviews
-from submit import submit
+from flask import Flask, redirect, request, url_for, render_template
+import movie_models as mm
 
 app = Flask(__name__)
+model = mm.get_model()
 
-app.add_url_rule('/', view_func=index.as_view('index'), methods=['GET', 'POST'])
-app.add_url_rule('/reviews', view_func=reviews.as_view('reviews'), methods=['GET'])
-app.add_url_rule('/submit', view_func=submit.as_view('submit'), methods=['GET', 'POST'])
+""" Function decorator using app.route ( '/', index() ). """
+@app.route('/')
+@app.route('/index.html', methods='GET', 'POST'])
+def index():
+	""" Renders landing page for movie review site. """
+	if request.method == 'GET':
+        return render_template('index.html', language=model.getLanguage())
+        
+     else:
+     	""" POST request to process form data to change the language of reviews. """
+        language = request.form['review_lang']
+        model.setLanguage(language)
+        return render_template('index.html', language=model.getLanguage())
+        
+@app.route('/reviews')
+def reviews():
+        """ Renders all reviews from the model. """
+        #databases must be unpacked before use in render template.
+        dbs = model.select()
+        return render_template('reviews.html', movies=dbs[0], reviews=dbs[1])
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+	""" Defines available genres for the a movie. Makes list available in GET and POST. """
+	
+	genres = ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy',
+        'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film Noir',
+        'History', 'Horror', 'Indie','Music', 'Musical', 'Mystery', 'Romance',
+         'Sci-Fi', 'Short', 'Sport', 'Superhero', 'Thiller', 'War', 'Western']
+         
+	if request.method =['GET']:
+        """ Renders submission form to submit a new movie review. """
+        return render_template('submit.html', genres = genres)
+
+    else:
+        """ POST request to process form data to insert new review into model.
+        Redirect to reviews page after submission. """
+        genre_selected = [g for g in genres if g in request.form]
+        model.insert(request.form['mov_name'], request.form['release_year'],
+        request.form['director'], request.form['mov_rating'],
+        request.form['runtime'], ','.join(genre_selected), request.form['review'],
+        request.form['rev_name'], request.form['rev_rating'])
+        return redirect(url_for('reviews'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
