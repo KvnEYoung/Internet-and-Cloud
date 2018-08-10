d513094a4b100a75642a241a2fbf6b251dca34a9
KevYoung <KevYoung@pdx.edu>
2018-08-09 22:26:01 -0700

    Fixing init bug.

--- final/movie_models/__init__.py
+++ final/movie_models/__init__.py
@@ -1,11 +1,11 @@
 #backend = 'sqlite'
 backend = 'cloudsql'
+
 def get_model():
-    if backend == 'sqlite':
-        from .model_sql import model
-    else if:
-        backend == 'cloudsql':
-        from .model_cloudsql import model
-    else:
-        raise ValueError("Invalid backend configuration")
-    return model()
+	if backend == 'sqlite':
+		from .model_sql import model
+	elif backend == 'cloudsql':
+		from .model_cloudsql import model
+	else:
+		raise ValueError("Invalid backend configuration.")
+	return model()
